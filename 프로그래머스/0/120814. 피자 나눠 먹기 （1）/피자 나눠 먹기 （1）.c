d803de2f6d6c6537c0c85d1b8c97811d77bffccd
김소희 <163392880+ttohee@users.noreply.github.com>
2024-09-05 21:39:34 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.41 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    answer = n / 7;
+    if(n % 7 >= 1)
+    {
+        answer++;
+    }
+    return answer;
+}
