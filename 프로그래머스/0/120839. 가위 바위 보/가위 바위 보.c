08ddc45f9a75e5605722a9133bac7619a4d910a1
김소희 <163392880+ttohee@users.noreply.github.com>
2024-09-16 21:26:21 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.44 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* rsp) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int len = strlen(rsp);
+    char* answer = (char*)malloc(len);
+    int i = 0;
+    for(i = 0; i < len; i++)
+    {
+        if(rsp[i] == '2')
+        {
+            answer[i] = '0';
+        }
+        else if(rsp[i] == '0')
+        {
+            answer[i] = '5';
+        }
+        else if(rsp[i] == '5')
+        {
+            answer[i] = '2';
+        }
+    }
+    answer[i] = NULL;
+    return answer;
+}
