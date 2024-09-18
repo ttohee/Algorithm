362b2fc2310e906047251f4264e64993f94523c7
김소희 <163392880+ttohee@users.noreply.github.com>
2024-09-18 14:17:58 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int t) {
+    int answer = 0;
+    for(int i = 0; i < t; i++)
+    {
+        n = n * 2;
+    }
+    answer = n;
+    return answer;
+}
