50f7f98e3d0d7269e2978ce998729965cd3d47d4
김소희 <163392880+ttohee@users.noreply.github.com>
2024-11-13 11:51:19 +0900

    [level 0] Title: 리스트 자르기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,7 +1,7 @@
 function solution(n, slicer, num_list) {
     let arr = [];
-    function my_slice(a,b,c){
-        for(let i = a; i <= b; i += c){
+    function my_slice(a = 0, b = num_list.length, c = 1){
+        for(let i = a; i < b; i += c){
             arr.push(num_list[i]);
         }
         return arr;
@@ -9,16 +9,16 @@ function solution(n, slicer, num_list) {
     
     switch(n){
         case 1:
-            arr = num_list.slice(0,slicer[1]+1);
+            arr = my_slice(0,slicer[1]+1);
             break;
         case 2:
-            arr = num_list.slice(slicer[0]);
+            arr = my_slice(slicer[0]);
             break;
         case 3:
-            arr = num_list.slice(slicer[0],slicer[1]+1);
+            arr = my_slice(slicer[0],slicer[1]+1);
             break;
         case 4:
-            arr = my_slice(slicer[0],slicer[1], slicer[2]);
+            arr = my_slice(slicer[0],slicer[1]+1, slicer[2]);
     }
     return arr;
 }
