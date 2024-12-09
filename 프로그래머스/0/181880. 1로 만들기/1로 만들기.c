93e978af5a9b3d8a78087472ba55d43fe6374fd2
김소희 <163392880+ttohee@users.noreply.github.com>
2024-12-09 19:25:26 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.c
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.c
@@ -9,11 +9,10 @@ int solution(int num_list[], size_t num_list_len) {
         while(num_list[i] != 1){
             if(num_list[i] % 2 == 0){
                 num_list[i] /= 2;
-                count++;
             } else if(num_list[i] % 2 != 0){
                 num_list[i] = (num_list[i] - 1) / 2;
-                count++;
             }
+            count++;
         }
     }
     return count;
